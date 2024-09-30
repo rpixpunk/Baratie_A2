6f80672fe46159a16867b3ca98bafdf880ae8f60
rpixpunk <rajiv.ramcharan2@gmail.com>
2024-09-30 19:16:47 +0000

    /

--- App/controllers/staff.py
+++ App/controllers/staff.py
@@ -3,6 +3,11 @@ from App.database import db
 from sqlalchemy.exc import IntegrityError
 
 def create_staff(name, role):
+    staff_member = Staff.query.filter_by(name=name, role=role).first()
+    if staff_member:
+       print("Staff member " + name + " is already assigned to " + role) 
+       return
+    
     staff = Staff(name=name, role=role)
     try:
         db.session.add(staff)
