ed7f204afe05c39d474977e1cf35bb1f125c9970
rpixpunk <rajiv.ramcharan2@gmail.com>
2024-10-09 04:03:46 +0000

    minor

--- App/views/courseAdmin.py
+++ App/views/courseAdmin.py
@@ -4,7 +4,6 @@ from flask_jwt_extended import jwt_required, current_user as jwt_current_user
 from.index import index_views
 from App.controllers import (
     create_course,
-    jwt_required,
     course_admin_required,
     view_course_staff,
     create_staff,
@@ -20,9 +19,7 @@ def create_staff_action():
     data = request.json # Return 400 Bad Request status code
     flash(f"Staff member {data['name']} created!")
     staff = create_staff(data['name'], data['role'])
-    if staff is None:
-        return jsonify({'message': f"Staff member {staff.name} already exists"}), 200
-    return jsonify({'message': f"Staff member {staff.name} created with id {staff.id}"}), 201
+    return jsonify({'message': f"Staff member successfully created with id {staff.id}"}), 201
 
 @course_admin_required
 @course_admin_views.route('/courses', methods=['POST'])
@@ -30,21 +27,21 @@ def create_course_action():
     data = request.json
     flash(f"Course {data['name']} created!")
     course = create_course(data['name'], data['description'])
-    return jsonify({'message': f"Course {course.name} created with id {course.id}"}), 201
+    return jsonify({'message': f"Course successfully created with id {course.id}"}), 201
 
 @course_admin_required
 @course_admin_views.route('/assign_staff', methods=['POST'])
 def assign_staff_view():
     data = request.json
     course_staff = assign_staff(data['course_name'], data['staff_name'])
-    return jsonify({'message': f"{data['staff_name']} with id {course_staff.staffID} assigned to {data['course_name']} with id {course_staff.courseID}"}), 201
+    return jsonify({'message': f"{data['staff_name']} with id {course_staff.staffID} successfully assigned to course with id {course_staff.courseID}"}), 201
 
 @course_admin_required
 @course_admin_views.route('/course_staff', methods=['GET'])
 def get_course_staff_view():
     data = request.json
     staff = view_course_staff(data['course_name'])
-    return jsonify(staff)
+    return jsonify(staff), 200
 
 
 
