87d16165acb8385efd8b29bd1b5c58048a60f0b9
rpixpunk <rajiv.ramcharan2@gmail.com>
2024-10-17 21:53:51 +0000

    fixed initial test

--- App/controllers/auth.py
+++ App/controllers/auth.py
@@ -3,12 +3,17 @@ from flask_jwt_extended import create_access_token, current_user, jwt_required, 
 
 from App.models import User, CourseAdmin
 
+def login(username, password):
+  user = User.query.filter_by(username=username).first()
+  if user and user.check_password(password):
+    return create_access_token(identity=username)
+  return None
+
 def course_admin_login(username, password):
   user = CourseAdmin.query.filter_by(username=username).first()
   if user and user.check_password(password):
     return create_access_token(identity=username)
   return None
-
 
 def setup_jwt(app):
   jwt = JWTManager(app)
